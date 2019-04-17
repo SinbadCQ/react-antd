025a5c16b567ede485e70510a1314c7ce086b49d
huxin <1228067812@qq.com>
2019-04-17 18:18:42 +0800

    antd

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,7 +5,9 @@ import { Button } from 'antd'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd'
 
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const {
+    Header, Content, Footer, Sider,
+} = Layout;
 
 export default class Home extends React.Component {
     render() {
@@ -28,53 +30,54 @@ export default class Home extends React.Component {
                             <Menu.Item key="3">nav 3</Menu.Item>
                         </Menu>
                     </Header>
-                    <Layout>
-                        <Sider width={200} style={{ background: '#fff' }}>
-                            <Menu
-                                mode="inline"
-                                defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
-                                style={{ height: '100%', borderRight: 0 }}
-                            >
-                                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                                    <Menu.Item key="5">option5</Menu.Item>
-                                    <Menu.Item key="6">option6</Menu.Item>
-                                    <Menu.Item key="7">option7</Menu.Item>
-                                    <Menu.Item key="8">option8</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                                    <Menu.Item key="9">option9</Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
-                                </SubMenu>
-                            </Menu>
-                        </Sider>
-                        <Layout style={{ padding: '0 24px 24px' }}>
-                            <Breadcrumb style={{ margin: '16px 0' }}>
-                                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                                <Breadcrumb.Item>List</Breadcrumb.Item>
-                                <Breadcrumb.Item>App</Breadcrumb.Item>
-                            </Breadcrumb>
-                            <Content style={{
-                                background: '#fff', padding: 24, margin: 0, minHeight: 280,
-                            }}
-                            >
-                                <Link to="/detail">
-                                    <Button type="primary">跳转详情</Button>
+                    <Content style={{ padding: '0 50px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                            <Sider width={200} style={{ background: '#fff' }}>
+                                <Menu
+                                    mode="inline"
+                                    defaultSelectedKeys={['1']}
+                                    defaultOpenKeys={['sub1']}
+                                    style={{ height: '100%' }}
+                                >
+                                    <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                                        <Menu.Item key="1">option1</Menu.Item>
+                                        <Menu.Item key="2">option2</Menu.Item>
+                                        <Menu.Item key="3">option3</Menu.Item>
+                                        <Menu.Item key="4">option4</Menu.Item>
+                                    </SubMenu>
+                                    <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                                        <Menu.Item key="5">option5</Menu.Item>
+                                        <Menu.Item key="6">option6</Menu.Item>
+                                        <Menu.Item key="7">option7</Menu.Item>
+                                        <Menu.Item key="8">option8</Menu.Item>
+                                    </SubMenu>
+                                    <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                                        <Menu.Item key="9">option9</Menu.Item>
+                                        <Menu.Item key="10">option10</Menu.Item>
+                                        <Menu.Item key="11">option11</Menu.Item>
+                                        <Menu.Item key="12">option12</Menu.Item>
+                                    </SubMenu>
+                                </Menu>
+                            </Sider>
+                            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                                <Link to="/carousel">
+                                    <Button>走马灯</Button>
                                 </Link>
+                                <Button type="primary">Primary</Button>
                                 <Button>Default</Button>
                                 <Button type="dashed">Dashed</Button>
                                 <Button type="danger">Danger</Button>
                             </Content>
                         </Layout>
-                    </Layout>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>
                 </Layout>
             </div>
         )
--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import loadable from './loadable'
 
-const routes = [{
+const routes = [
+    {
         path: '/',
         component: loadable(() => import('./pages/index/index'))
     },
     {
         path: '/detail',
         component: loadable(() => import('./pages/detail/index'))
-    }];
+    },
+    {
+        path: '/carousel',
+        component: loadable(() => import('./pages/carousel/index'))
+    },
+];
 
 export default routes
